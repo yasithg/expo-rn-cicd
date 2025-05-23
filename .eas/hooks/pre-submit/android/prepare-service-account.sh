29efe9c228a64eb7ce2584cb9165245f0de90cb6
yasithg <yasith@smashtaps.com>
2025-05-23 15:08:35 +0530

    update script

--- .eas/hooks/pre-submit/android/prepare-service-account.sh
+++ .eas/hooks/pre-submit/android/prepare-service-account.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "=== DEBUG: Starting pre-submit hook ==="
+
+
 echo "Preparing Google Play service account JSON..."
 
 # Check if the env var exists
@@ -10,3 +13,7 @@ fi
 
 # Decode and save to file
 echo $ANDROID_SERVICE_ACCOUNT | base64 -d > service-account.json
+
+ls -l service-account.json
+cat service-account.json | head -n 5
+echo "=== DEBUG: Finished pre-submit hook ==="
