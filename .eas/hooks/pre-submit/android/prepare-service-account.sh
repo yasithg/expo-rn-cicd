568a223c90a81564256dcb52513c324279243075
yasithg <yasith@smashtaps.com>
2025-05-26 13:30:32 +0530

    change file

--- .eas/hooks/pre-submit/android/prepare-service-account.sh
+++ .eas/hooks/pre-submit/android/prepare-service-account.sh
@@ -6,13 +6,13 @@ echo "=== DEBUG: Starting pre-submit hook ==="
 echo "Preparing Google Play service account JSON..."
 
 # Check if the env var exists
-if [ -z "$ANDROID_SERVICE_ACCOUNT_NEW" ]; then
-  echo "Error: ANDROID_SERVICE_ACCOUNT_NEW is not set"
+if [ -z "$ANDROID_SERVICE_ACCOUNT" ]; then
+  echo "Error: ANDROID_SERVICE_ACCOUNT is not set"
   exit 1
 fi
 
 # Decode and save to file
-echo $ANDROID_SERVICE_ACCOUNT_NEW | base64 -d > service-account.json
+echo $ANDROID_SERVICE_ACCOUNT | base64 -d > service-account.json
 
 ls -l service-account.json
 cat service-account.json | head -n 5
